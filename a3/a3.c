bc06906e9422982efecae2be92ed34f8a93319fc
Dana550 <danapode151@gmail.com>
2023-05-27 12:52:20 +0300

    fixing connection

--- a3/a3.c
+++ a3/a3.c
@@ -6,77 +6,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define REQ_PIPE_NAME "REQ_PIPE_45790"
 #define RESP_PIPE_NAME "RESP_PIPE_45790"
+#define REQ_PIPE_NAME "REQ_PIPE_45790"
 
-/*
-#define MAX_STRING_SIZE 250
-
-typedef struct {
-    char string_param[MAX_STRING_SIZE];
-} Request;
-
-typedef struct {
-    unsigned int number_param;
-    char string_param[MAX_STRING_SIZE];
-} Response;
-
-void sendMessage(int pipe, const void* message, size_t size) {
-    write(pipe, message, size);
-}
-
-void receiveMessage(int pipe, void* buffer, size_t size) {
-    read(pipe, buffer, size);
-}
-*/
 
 int main() {
     int respPipe=-1, reqPipe=-1;
-    //Request request;
-   // Response response;
-   // strcpy(request.string_param,"PING");
-   // strcpy(response.string_param,"PING PONG 45790");
-
-   
+  
     //create respPipe
     if (mkfifo(RESP_PIPE_NAME, 0600) !=0) {
-        printf("ERROR\ncannot create the response pipe\n");
-        return 1;
+        printf("ERROR\ncannot create the response pipe");
+        return 2;
     }
 
-    //open the reqPipe
     reqPipe = open(REQ_PIPE_NAME, O_RDONLY);
-    if (reqPipe == -1) {
-        printf("ERROR\ncannot open the request pipe\n");
-        //unlink(RESP_PIPE_NAME); 
+    if (reqPipe == -1) 
+    {
+        printf("ERROR\ncannot open the request pipe");
         return 1;
     }
 
-    //open respPipe 
     respPipe = open(RESP_PIPE_NAME, O_WRONLY);
-    if (respPipe == -1) {
-        close(reqPipe);
-        //unlink(RESP_PIPE_NAME);  
+    if (respPipe == -1) 
+    {
         return 1;
     }
 
     //write in pipe-ul resp
-    write(respPipe,"5HELLO",6);
+    char string[7]="HELLO";
+    unsigned int length=strlen(string);
+    write(respPipe, &length, 1);
+    write(respPipe,string,length);    
     printf("SUCCESS\n");
-    
+
+    unlink(REQ_PIPE_NAME);
+    unlink(RESP_PIPE_NAME);
  
-    /*
-    while (1) {
-        // Receive request
-        receiveMessage(reqPipe, &request, sizeof(Request));
-         if(strcmp(&request,"PING") == 0)
-        {
-            // Write back response
-            sendMessage(respPipe, &response, sizeof(Response));
-            break;
-        }
-    }*/
-
-
     return 0;
 }
